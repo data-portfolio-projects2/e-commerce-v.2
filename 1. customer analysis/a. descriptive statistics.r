5d3845fa05fd98a3dd8a2a5e6848379c532659db
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 23:32:48 +0800

    Update a. descriptive statistics.r

--- 1. customer analysis/a. descriptive statistics.r
+++ 1. customer analysis/a. descriptive statistics.r
@@ -1,35 +1,50 @@
 library(dplyr)
 
-set.seed(123)  
+set.seed(123)
+
 demographics <- data.frame(
-  Age = sample(18:65, 10, replace = TRUE),
-  Location = sample(c("Urban", "Suburban", "Rural"), 10, replace = TRUE),
-  Gender = sample(c("Male", "Female"), 10, replace = TRUE),
+  Age          = sample(18:65, 10, replace = TRUE),
+  Location     = sample(c("Urban", "Suburban", "Rural"), 10, replace = TRUE),
+  Gender       = sample(c("Male", "Female"), 10, replace = TRUE),
   Income_Level = sample(20000:100000, 10, replace = TRUE)
 )
 
-print("Simulated Demographics Data")
-print(demographics)
+EDA <- setRefClass("EDA",
+                   fields = list(data = "data.frame"),
+                   methods = list(
+                     print_data = function() {
+                       print("Simulated Demographics Data")
+                       print(data)
+                     },
+                     
+                     descriptive_stats = function() {
+                       summarise(data,
+                                 Avg_Age       = mean(Age),
+                                 Median_Age    = median(Age),
+                                 Age_SD        = sd(Age),
+                                 Avg_Income    = mean(Income_Level),
+                                 Median_Income = median(Income_Level),
+                                 Income_SD     = sd(Income_Level))
+                     },
+                     
+                     summary_categorical = function() {
+                       list(
+                         Location_Distribution = table(data$Location),
+                         Gender_Distribution   = table(data$Gender)
+                       )
+                     }
+                   ))
 
-descriptive_stats <- demographics %>%
-  summarise(
-    Avg_Age = mean(Age),
-    Median_Age = median(Age),
-    Age_SD = sd(Age),
-    Avg_Income = mean(Income_Level),
-    Median_Income = median(Income_Level),
-    Income_SD = sd(Income_Level)
-  )
+eda <- EDA$new(data = demographics)
 
+eda$print_data()
+descriptive_stats <- eda$descriptive_stats()
 print("Descriptive Statistics")
 print(descriptive_stats)
 
-# Summary of Categorical Variables
-location_summary <- table(demographics$Location)
-gender_summary <- table(demographics$Gender)
-
+categorical_summary <- eda$summary_categorical()
 print("Location Distribution")
-print(location_summary)
+print(categorical_summary$Location_Distribution)
 
 print("Gender Distribution")
-print(gender_summary)
+print(categorical_summary$Gender_Distribution)
