94227a9eae307562f8e344c6ec8d339d8b3542c6
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-26 00:00:41 +0800

    Update d. time series analysis.r

--- 4. purchase behavior/d. time series analysis.r
+++ 4. purchase behavior/d. time series analysis.r
@@ -1,43 +1,57 @@
 library(forecast)
 library(tseries)
-library(ggplot2)
 
-set.seed(123)
-sales_data <- data.frame(
-  date = seq.Date(from = as.Date('2022-01-01'), by = 'day', length.out = 180),
-  sales = rnorm(180, mean = 200, sd = 50) + sin(1:180/10) * 50
-)
-
-ggplot(sales_data, aes(x = date, y = sales)) +
-  geom_line() +
-  ggtitle("Simulated Sales Data") +
-  xlab("Date") +
-  ylab("Sales") +
-  theme_minimal()
-
-sales_ts <- ts(sales_data$sales, frequency = 365)
-
-decomposed_sales <- decompose(sales_ts)
-plot(decomposed_sales)
-
-adf_test <- adf.test(sales_ts)
-print(adf_test)
-
-if(adf_test$p.value > 0.05) {
-  sales_ts <- diff(sales_ts, differences = 1)
+generate_sales_data <- function() {
+  set.seed(123)
+  data.frame(
+    date = seq.Date(from = as.Date('2022-01-01'), by = 'day', length.out = 180),
+    sales = rnorm(180, mean = 200, sd = 50) + sin(1:180/10) * 50
+  )
 }
 
-arima_model <- auto.arima(sales_ts)
-forecast_sales <- forecast(arima_model, h = 30)
+plot_sales_data <- function(sales_data) {
+  ggplot(sales_data, aes(x = date, y = sales)) +
+    geom_line() +
+    ggtitle("Simulated Sales Data") +
+    xlab("Date") +
+    ylab("Sales") +
+    theme_minimal()
+}
 
-plot(forecast_sales, main = "Sales Forecast (Next 30 Days)")
+convert_to_time_series <- function(sales_data) {
+  ts(sales_data$sales, frequency = 365)
+}
 
-seasonal_component <- decomposed_sales$seasonal
-ggplot(data = sales_data, aes(x = date, y = seasonal_component)) +
-  geom_line() +
-  ggtitle("Seasonal Component of Sales") +
-  xlab("Date") +
-  ylab("Seasonality Effect") +
-  theme_minimal()
+decompose_sales <- function(sales_ts) {
+  decompose(sales_ts)
+}
 
-print(forecast_sales)
+perform_adf_test <- function(sales_ts) {
+  adf.test(sales_ts)
+}
+
+apply_differencing <- function(sales_ts) {
+  diff(sales_ts, differences = 1)
+}
+
+fit_arima_model <- function(sales_ts) {
+  auto.arima(sales_ts)
+}
+
+forecast_sales_data <- function(arima_model) {
+  forecast(arima_model, h = 30)
+}
+
+plot_forecast <- function(forecast_sales) {
+  plot(forecast_sales, main = "Sales Forecast (Next 30 Days)")
+}
+
+plot_seasonal_component <- function(sales_data, seasonal_component) {
+  ggplot(data = sales_data, aes(x = date, y = seasonal_component)) +
+    geom_line() +
+    ggtitle("Seasonal Component of Sales") +
+    xlab("Date") +
+    ylab("Seasonality Effect") +
+    theme_minimal()
+}
+
