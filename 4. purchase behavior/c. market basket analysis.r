b3d79b82c7735ba28d17d57b5c78fbf3357f39fd
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 23:59:37 +0800

    Update c. market basket analysis.r

--- 4. purchase behavior/c. market basket analysis.r
+++ 4. purchase behavior/c. market basket analysis.r
@@ -1,33 +1,32 @@
-if (!require(arules)) install.packages("arules", dependencies = TRUE)
-
 library(arules)
 
-set.seed(123)
-n_customers <- 10
-n_transactions <- 50
+generate_transaction_data <- function(n_customers, n_transactions, products) {
+  data.frame(
+    customer_id = sample(1:n_customers, n_transactions, replace = TRUE),
+    transaction_date = sample(seq.Date(from = as.Date('2022-01-01'), to = Sys.Date(), by = 'day'), n_transactions, replace = TRUE),
+    product = sample(products, n_transactions, replace = TRUE)
+  )
+}
 
-products <- c("Product A", "Product B", "Product C", "Product D", "Product E")
-transaction_data <- data.frame(
-  customer_id = sample(1:n_customers, n_transactions, replace = TRUE),
-  transaction_date = sample(seq.Date(from = as.Date('2022-01-01'), to = Sys.Date(), by = 'day'), n_transactions, replace = TRUE),
-  product = sample(products, n_transactions, replace = TRUE)
-)
+create_transaction_list <- function(transaction_data) {
+  transaction_list <- split(transaction_data$product, transaction_data$customer_id)
+  lapply(transaction_list, unique)
+}
 
-transaction_list <- split(transaction_data$product, transaction_data$customer_id)
-transaction_list <- lapply(transaction_list, unique)
-transactions <- as(transaction_list, "transactions")
+convert_to_transactions <- function(transaction_list) {
+  as(transaction_list, "transactions")
+}
 
-summary(transactions)
+generate_association_rules <- function(transactions) {
+  apriori(transactions, parameter = list(support = 0.2, confidence = 0.6, target = "rules"))
+}
 
-rules <- apriori(transactions, parameter = list(support = 0.2, confidence = 0.6, target = "rules"))
+filter_rules_by_lift <- function(rules) {
+  subset(rules, lift > 1)
+}
 
-inspect(rules)
+visualize_rules <- function(rules) {
+  library(arulesViz)
+  plot(rules, method = "graph", main = "Market Basket Analysis - Association Rules")
+}
 
-lift_rules <- subset(rules, lift > 1)
-inspect(lift_rules)
-
-library(arulesViz)
-plot(rules, method = "graph", main = "Market Basket Analysis - Association Rules")
-
-top_rules <- sort(rules, by = "lift", decreasing = TRUE)
-inspect(head(top_rules, 10))
