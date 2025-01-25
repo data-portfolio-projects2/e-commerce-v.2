821a0beba9c7e5f8d0249d4c7a9efdecfea30a2c
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 23:49:00 +0800

    Update b. thematic analysis.r

--- 2. customer feedback/b. thematic analysis.r
+++ 2. customer feedback/b. thematic analysis.r
@@ -1,23 +1,30 @@
-library(tm)
-library(wordcloud)
-library(tidyverse)
+load_required_libraries <- function() {
+  library(tm)
+  library(wordcloud)
+  library(tidyverse)
+}
 
-feedback_data_cleaned <- feedback_data %>%
-  mutate(
-    Feedback_Cleaned = Feedback %>%
-      tolower() %>%
-      removePunctuation() %>%
-      removeNumbers() %>%
-      removeWords(stopwords("en"))
-  )
+clean_feedback_data <- function(feedback_data) {
+  feedback_data_cleaned <- feedback_data %>%
+    mutate(
+      Feedback_Cleaned = Feedback %>%
+        tolower() %>%
+        removePunctuation() %>%
+        removeNumbers() %>%
+        removeWords(stopwords("en"))
+    )
+  return(feedback_data_cleaned)
+}
 
-feedback_words <- unlist(str_split(feedback_data_cleaned$Feedback_Cleaned, " "))
-word_freq <- table(feedback_words)
-word_freq <- word_freq[word_freq > 0]
+generate_wordcloud <- function(feedback_data_cleaned) {
+  feedback_words <- unlist(str_split(feedback_data_cleaned$Feedback_Cleaned, " "))
+  word_freq <- table(feedback_words)
+  word_freq <- word_freq[word_freq > 0]
 
-wordcloud(names(word_freq), freq = word_freq, min.freq = 1, scale = c(3, 0.5), 
-          colors = brewer.pal(8, "Dark2"))
-
-word_freq_sorted <- sort(word_freq, decreasing = TRUE)
-cat("Most frequent words:\n")
-print(head(word_freq_sorted, 10))
+  wordcloud(names(word_freq), freq = word_freq, min.freq = 1, scale = c(3, 0.5), 
+            colors = brewer.pal(8, "Dark2"))
+  
+  word_freq_sorted <- sort(word_freq, decreasing = TRUE)
+  cat("Most frequent words:\n")
+  print(head(word_freq_sorted, 10))
+}
