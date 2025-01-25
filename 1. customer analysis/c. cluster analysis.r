0ab9afc5b6aecfdc4b3300d8055988a123e85ce0
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-01-25 23:35:34 +0800

    Update c. cluster analysis.r

--- 1. customer analysis/c. cluster analysis.r
+++ 1. customer analysis/c. cluster analysis.r
@@ -1,27 +1,53 @@
-set.seed(123)  
-demographics <- data.frame(
-  Age = sample(18:65, 10, replace = TRUE),
-  Location = sample(c("Urban", "Suburban", "Rural"), 10, replace = TRUE),
-  Gender = sample(c("Male", "Female"), 10, replace = TRUE),
-  Income_Level = sample(20000:100000, 10, replace = TRUE)
-)
-
+library(dplyr)
 library(cluster)
 library(factoextra)
 
-clustering_data <- demographics[, c("Age", "Income_Level")]
-scaled_data <- scale(clustering_data)
 set.seed(123)
-kmeans_result <- kmeans(scaled_data, centers = 3, nstart = 25)
-demographics$Cluster <- as.factor(kmeans_result$cluster)
 
-fviz_cluster(kmeans_result, data = scaled_data,
-             geom = "point", 
-             ellipse.type = "norm",
-             ggtheme = theme_minimal(),
-             main = "Customer Segmentation by Age and Income Level")
+create_demographics_data <- function() {
+  data.frame(
+    Age = sample(18:65, 10, replace = TRUE),
+    Location = sample(c("Urban", "Suburban", "Rural"), 10, replace = TRUE),
+    Gender = sample(c("Male", "Female"), 10, replace = TRUE),
+    Income_Level = sample(20000:100000, 10, replace = TRUE)
+  )
+}
 
-cluster_profiles <- aggregate(demographics[, c("Age", "Income_Level")], 
-                               by = list(Cluster = demographics$Cluster), 
-                               FUN = mean)
-print(cluster_profiles)
+perform_kmeans_clustering <- function(data, centers = 3) {
+  clustering_data <- data[, c("Age", "Income_Level")]
+  scaled_data <- scale(clustering_data)
+  kmeans_result <- kmeans(scaled_data, centers = centers, nstart = 25)
+  return(kmeans_result)
+}
+
+add_cluster_column <- function(data, kmeans_result) {
+  data$Cluster <- as.factor(kmeans_result$cluster)
+  return(data)
+}
+
+visualize_clusters <- function(kmeans_result, scaled_data) {
+  fviz_cluster(kmeans_result, data = scaled_data,
+               geom = "point", 
+               ellipse.type = "norm",
+               ggtheme = theme_minimal(),
+               main = "Customer Segmentation by Age and Income Level")
+}
+
+calculate_cluster_profiles <- function(data) {
+  cluster_profiles <- aggregate(data[, c("Age", "Income_Level")], 
+                                by = list(Cluster = data$Cluster), 
+                                FUN = mean)
+  return(cluster_profiles)
+}
+
+run_eda_pipeline <- function() {
+  demographics <- create_demographics_data()
+  kmeans_result <- perform_kmeans_clustering(demographics)
+  demographics <- add_cluster_column(demographics, kmeans_result)
+  scaled_data <- scale(demographics[, c("Age", "Income_Level")])
+  visualize_clusters(kmeans_result, scaled_data)
+  cluster_profiles <- calculate_cluster_profiles(demographics)
+  print(cluster_profiles)
+}
+
+run_eda_pipeline()
