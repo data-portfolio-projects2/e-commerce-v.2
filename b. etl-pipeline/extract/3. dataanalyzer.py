cc3949cb1dddac7e08577a67de575051d0b8c5d3
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-11 00:02:22 +0800

    Create 3. dataanalyzer.py

---
+++ b. etl-pipeline/extract/3. dataanalyzer.py
@@ -0,0 +1,10 @@
+def __init__(self, df):
+
+def preview_data(self):
+        """Preview the data"""
+
+def check_missing(self):
+        """Check for missing data"""
+
+def check_duplicates(self):
+        """Check for duplicate data"""
