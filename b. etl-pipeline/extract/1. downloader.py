a0b96704a59715d5294c53d6e18e4d7e08e54052
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-10 23:57:06 +0800

    Create 1. downloader.py

---
+++ b. etl-pipeline/extract/1. downloader.py
@@ -0,0 +1,9 @@
+def authenticate_kaggle(self):
+        """Authenticate Kaggle API"""
+
+def initiate_os(self):
+        """Create download directory if it doesn't exist"""
+
+def download_data(self, dataset_name, download_dir):
+        """Downloads the dataset from Kaggle"""
+
