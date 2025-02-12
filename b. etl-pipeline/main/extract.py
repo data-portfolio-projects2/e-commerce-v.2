bd2877f79ecc2ec70a51be648c4ecaac3338cc29
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-12 12:07:54 +0800

    Update extract.py

--- b. etl-pipeline/main/extract.py
+++ b. etl-pipeline/main/extract.py
@@ -1,19 +1,4 @@
-if __name__ == "__main__":
+from orchestrator import Extract
 
-from extract import DataDownloader
-from extract import CSVExtractor
-from extract import DataAnalyzer
-from extract import DataSaver
-
-downloader = DataDownloader("data")
-downloader()
-
-extract = CSVExtractor("data")
-df = extract.extract_csv(file_path)
-
-analyze = DataAnalyzer(df)
-analyze.preview_data()
-missing_rows = analyze.check_missing()
-
-saver = DataSaver(missing_rows)
-saver.save_missing()
+extractor = Extract()
+extractor.call_extract()
