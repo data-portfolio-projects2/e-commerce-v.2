55b8c5a2c5d37a1e93a41492129b6d54ac3f3e5b
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-12 21:43:26 +0800

    Create 1. datatransformer.py

---
+++ b. etl-pipeline/transform/1. datatransformer.py
@@ -0,0 +1,15 @@
+@time_execution
+def extract_df(self, file_path):
+  """Extract dask data"""
+
+@time_execution
+def lowercase_col(self, df):
+  """Transform columns into lowercase"""
+
+@time_execution
+def strip_space(self, df):
+  """Remove trailing spaces in columns"""
+
+@time_execution
+def replace_(self, df):
+  """Replacing space w/ underscore"""
