1c7902994a4286b955a06cfab56c45a8c4b619de
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-12 18:35:07 +0800

    Update orchestrator.py

--- b. etl-pipeline/orchestrator/orchestrator.py
+++ b. etl-pipeline/orchestrator/orchestrator.py
@@ -1,27 +1,45 @@
 # Import extract
-from credentials import load
-from credentials import path
 from extract import DataDownloader
 from extract import CSVExtractor
 from extract import DataAnalyzer
 from extract import DataSaver
+from credentials import path
 
 class Extract:
 
-    def call_extract(self):
+    def download(self):
         downloader = DataDownloader()
         downloader()
+    
+    def extract(self):
+        extractor = CSVExtractor(path.csv)
+        df = extractor.extract_csv()
+        return df
 
-        extractor = CSVExtractor()
-        df = extractor.extract_csv(path.csv)
+    def savedf(self, df):
+        df.to_csv(path.df, index=False)
+        return df
 
+    def analyze(self, df):
         analyzer = DataAnalyzer(df)
         analyzer.preview_data()
         missing_rows = analyzer.check_missing()
+        return missing_rows
 
+    def save(self, missing_rows):
         saver = DataSaver(missing_rows)
-        saver()
+        saver() 
 
-if __name__ == "__main__":
-    extract = Extract()
-    extract.call_extract()
+    def __call__(self):
+        self.download()
+        df = self.extract()
+        df = df.compute()
+        df = self.savedf(df)
+        df = self.analyze(df)
+        self.save(df)
+
+#class Transform: 
+    
+#    def call_load(self):
+#        pass
+
