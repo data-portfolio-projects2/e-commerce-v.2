c081075e22dfa9aba62b1eb5028e5eb2900a396d
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-12 12:12:22 +0800

    Update orchestrator.py

--- b. etl-pipeline/main/orchestrator.py
+++ b. etl-pipeline/main/orchestrator.py
@@ -1,29 +1,4 @@
-# Import extract
-from credentials import load
-from credentials import path
-from extract import DataDownloader
-from extract import CSVExtractor
-from extract import DataAnalyzer
-from extract import DataSaver
+from orchestrator import Extract
 
-class Extract:
-
-    def call_extract(self):
-        downloader = DataDownloader()
-        downloader()
-
-        extractor = CSVExtractor()
-        df = extractor.extract_csv(path.csv)
-
-        analyzer = DataAnalyzer(df)
-        analyzer.preview_data()
-        missing_rows = analyzer.check_missing()
-
-        saver = DataSaver(missing_rows)
-        saver()
-
-if __name__ == "__main__":
-    extract = Extract()
-    extract.call_extract()
-
-
+extractor = Extract()
+extractor.call_extract()
