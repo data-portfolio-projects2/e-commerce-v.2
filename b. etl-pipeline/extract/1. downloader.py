9ad191e3461c971d68d65622328829da8035928b
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-14 03:52:17 +0800

    Update 1. downloader.py

--- b. etl-pipeline/extract/1. downloader.py
+++ b. etl-pipeline/extract/1. downloader.py
@@ -1,12 +1,15 @@
-@time_execution
-def authenticate_kaggle(self):
+class DataDownloader(metaclass=Static):
+ 
+    @time_execution
+    def authenticate_kaggle():
         """Authenticate Kaggle API"""
-
-def initiate_os(self):
+    
+    def initiate_os():
         """Create download directory if it doesn't exist"""
-
-@time_execution
-def download_data(self, dataset_name, download_dir):
+    
+    @time_execution
+    @memory_usage
+    def download_data(dataset_name, download_dir):
         """Downloads the dataset from Kaggle"""
-
-def __call__(self):
+    
+    def __call__(self): 
