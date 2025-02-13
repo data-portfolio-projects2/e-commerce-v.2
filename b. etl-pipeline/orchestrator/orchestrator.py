7cf4b1ad9c289483ed9442837b08810e2d666ad5
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-14 03:43:20 +0800

    Update orchestrator.py

--- b. etl-pipeline/orchestrator/orchestrator.py
+++ b. etl-pipeline/orchestrator/orchestrator.py
@@ -1,74 +1,80 @@
-# Import extract
-from extract import DataDownloader
-from extract import CSVExtractor
-from extract import DataAnalyzer
-from extract import DataSaver
+from extract import DataDownloader, CSVExtractor, DataAnalyzer, DataSaver
 from transform import DataTransformer
 from credentials import path
+from monitor import Static
 
-class Extract:
 
-    def download(self):
-        downloader = DataDownloader()
-        downloader()
-    
-    def extract(self, file_path):
-        extractor = CSVExtractor(file_path)
-        df = extractor.extract_csv()
+class Extract(metaclass=Static):
+
+    def download():
+        download = DataDownloader()
+        download()
+
+    def extract():
+        df = CSVExtractor.extract_csv()
         return df
-
-    def savedf(self, df):
+        
+    def savedf(df):
         df.to_csv(path.df, index=False)
         return df
 
-    def analyze(self, df):
-        analyzer = DataAnalyzer(df)
-        analyzer.preview_data()
-        missing_rows = analyzer.check_missing()
-        return missing_rows
+    def analyze(df):
+        DataAnalyzer.preview_data(df)
+        df = DataAnalyzer.check_missing(df)
+        return df
 
-    def save(self, data):
-        saver = DataSaver(data)
-        saver.save_missing()
+    def save(df):
+        return DataSaver.save_missing(df)
 
     def __call__(self):
         self.download()
-        df = self.extract(path.csv)
+        df = self.extract()
         df = df.compute()
         df = self.savedf(df)
         df = self.analyze(df)
         self.save(df)
 
 
-class Transform: 
+class Transform(metaclass=Static): 
     
-    def df_extract(self, file_path):
-        dt = DataTransformer()
-        extr = dt.extract_df(file_path)
-        extr = extr.drop(['Add-ons Purchased'], axis=1)
-        return extr
+    def df_extract(file_path):
+        df = DataTransformer.extract_df(file_path)
+        df = df.drop(['Add-ons Purchased'], axis=1)
+        return df
     
-    def col_lowercase(self, df):
-        dt = DataTransformer()
-        lower = dt.lowercase_col(df)
-        return lower
+    def to_string(df):
+        df = DataTransformer.to_string(df)
+        return df
     
-    def col_remove_space(self, df):
-        dt = DataTransformer()
-        strip = dt.strip_space(df)
-        return strip
+    def col_lowercase(df):
+        df = DataTransformer.lowercase_col(df)
+        return df
     
-    def col_replace_(self, df):
-        dt = DataTransformer()
-        replace_ = dt.replace_(df)
-        return replace_
+    def col_remove_space(df):
+        df = DataTransformer.strip_space(df)
+        return df
+    
+    def col_replace_(df):
+        df = DataTransformer.replace_(df)
+        return df
+    
+    def savedf(df):
+        df.to_csv(path.df, index=False)
+        return df
     
     def __call__(self):
-        df = self.df_extract(path.df)
+        df = self.df_extract(path.csv)
         df = df.compute()
+        #df = self.to_string(df)
         df = self.col_lowercase(df)
         df = self.col_remove_space(df)
         df = self.col_replace_(df)
+        df = self.savedf(df)
 
 
         
+
+
+
+        
+
