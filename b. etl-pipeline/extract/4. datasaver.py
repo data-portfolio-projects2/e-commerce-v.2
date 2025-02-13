9334507823abb074c5d3309f4388ad3324bc6214
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-14 04:02:52 +0800

    Update 4. datasaver.py

--- b. etl-pipeline/extract/4. datasaver.py
+++ b. etl-pipeline/extract/4. datasaver.py
@@ -1,7 +1,7 @@
-def __init__(self, missing_rows):
+class DataSaver(metaclass=Static):
 
-def save_missing(self):
-        """Save missing_data to a CSV file"""
-
-def save_duplicates(self):
-        """Save duplicates to a CSV file"""
+        def save_missing(missing_rows):
+            """Save missing_data to a CSV file"""
+        
+        def save_duplicates(duplicate_rows):
+            """Save duplicates to a CSV file"""
