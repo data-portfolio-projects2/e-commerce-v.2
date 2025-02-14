6fdb55d18f2579282292a3062da3e63596195f65
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-14 16:27:14 +0800

    Create product.py

---
+++ b. etl-pipeline/credentials/product.py
@@ -0,0 +1,6 @@
+class product:
+    """Storing data for validation"""
+  
+    p_type = {'Smartphone', 'Tablet', 'Laptop', 'Smartwatch', 'Headphones'}
+    sku = {'SKU1004', 'SKU1002', 'SKU1005', 'SKU1001', 'SKU1003', 'LTP123',
+        'SMP234', 'TBL345', 'HDP456', 'SWT567'}
