f62d09de83cdfdb78f36557110a280a6f37aba2b
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-15 17:23:57 +0800

    Delete product.py

--- b. etl-pipeline/credentials/product.py
+++
@@ -1,6 +0,0 @@
-class product:
-    """Product type validation"""
-  
-    p_type = {'Smartphone', 'Tablet', 'Laptop', 'Smartwatch', 'Headphones'}
-    sku = {'SKU1004', 'SKU1002', 'SKU1005', 'SKU1001', 'SKU1003', 'LTP123',
-        'SMP234', 'TBL345', 'HDP456', 'SWT567'}
