c1795a464faa1a01e527dac9af4128b0d5411db1
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-17 21:54:07 +0800

    Create import.py

---
+++ b. etl-pipeline/abstract/import.py
@@ -0,0 +1,6 @@
+from transform import DataTransformer as transform
+from validate import DataValidator as validate
+from extract import DataExtractor as extract
+from download import DataDownloader as load
+from save import DataSaver as save
+from monitor import static
