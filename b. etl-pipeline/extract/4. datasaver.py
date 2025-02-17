a27282b2f61f8d64fdb8158347a94a20fd4f07e0
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-17 21:45:21 +0800

    Delete 4. datasaver.py

--- b. etl-pipeline/extract/4. datasaver.py
+++
@@ -1,7 +0,0 @@
-class DataSaver(metaclass=Static):
-
-        def save_missing(missing_rows):
-            """Save missing_data to a CSV file"""
-        
-        def save_duplicates(duplicate_rows):
-            """Save duplicates to a CSV file"""
