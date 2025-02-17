a69d9ca2997385384dd1506b06f8ce2e6208b692
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-17 21:43:47 +0800

    Create download.py

---
+++ b. etl-pipeline/download/download.py
@@ -0,0 +1,11 @@
+class DataDownloader(metaclass=static):
+
+    def authenticate_kaggle():
+        """Authenticate Kaggle API"""
+    
+    def initiate_os():
+        """Create download directory if it doesn't exist"""
+
+    def download_data():
+        """Downloads the dataset from Kaggle"""
+
