35a038ef98d402a31a61a561bbd7dfe81c6eb28f
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-17 21:52:09 +0800

    Update import.py

--- b. etl-pipeline/orchestrator/import.py
+++ b. etl-pipeline/orchestrator/import.py
@@ -1,4 +1,2 @@
-from extract import DataDownloader as load, CSVExtractor as csv, DataAnalyzer as analyze, DataSaver as save
-from transform import DataTransformer as transform
-from credentials import path
-from monitor import static 
+from monitor import time_execution as time, memory_usage as memory, static
+from abstract import Downloader as download, Transformer as transform, Validator as validate
