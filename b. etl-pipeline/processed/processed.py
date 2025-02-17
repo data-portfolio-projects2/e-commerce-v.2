0eedc5cc4201e13eda76cad99f7b2f3ed6698b34
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-17 21:39:50 +0800

    Create processed.py

---
+++ b. etl-pipeline/processed/processed.py
@@ -0,0 +1,53 @@
+import dask.dataframe as dd
+from monitor import static
+
+class processed(metaclass=static):
+
+    def add_on_total():
+        return dd.read_csv(add-on_total.csv, dtype=str)
+
+    def age():
+        return dd.read_csv(age.csv", dtype=str)
+    
+    def customer_id():
+        return dd.read_csv"customer_id.csv, dtype=str)
+
+    def gender():
+        return dd.read_csv(gender.csv, dtype=str)
+    
+    def membership():
+        return dd.read_csv(loyalty_member.csv, dtype=str)
+    
+    def order_status():
+        return dd.read_csv(order_status.csv, dtype=str)
+    
+    def payment_method():
+        return dd.read_csv(payment_method.csv, dtype=str)
+    
+    def products():
+        return dd.read_csv(product_type.csv, dtype=str)
+    
+    def purchase_date():
+        return dd.read_csv(purchase_date.csv, dtype=str)
+    
+    def quantity():
+        return dd.read_csv(quantity.csv, dtype=str)
+    
+    def rating():
+        return dd.read_csv(rating.csv, dtype=str)
+    
+    def shipping_type():
+        return dd.read_csv(shipping_type.csv, dtype=str)
+    
+    def sku():
+        return dd.read_csv(sku.csv, dtype=str)
+    
+    def total_price():
+        return dd.read_csv(total_price.csv, dtype=str)
+    
+    def unit_price():
+        return dd.read_csv(unit_price.csv, dtype=str)
+
+
+
+
