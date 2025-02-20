320dd0ee83b46d61fc82c504b05c2d1b40deec6a
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 04:04:06 +0800

    Update import.py

--- b. etl-pipeline/abstract/import.py
+++ b. etl-pipeline/abstract/import.py
@@ -1,6 +1,7 @@
 from transform import DataTransformer as transform
 from validate import DataValidator as validate
 from extract import DataExtractor as extract
-from download import DataDownloader as load
+from download import DataDownloader as download
 from save import DataSaver as save
+from load import Loader as load
 from monitor import static
