2cb5062770cb9100b1793f26d8027a12aae438a5
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 03:54:14 +0800

    Update validate.py

--- b. etl-pipeline/validate/validate.py
+++ b. etl-pipeline/validate/validate.py
@@ -2,45 +2,57 @@ class DataValidator(metaclass=static):
 
     def validate_product():
         """Validates product type"""
-    
+       
+
     def validate_sku():
         """Validates skus"""
+       
     
     def validate_order_status():
         """Validates order status"""
+   
     
     def validate_shipping():
         """Validates shipping type"""
+       
         
     def validate_payment():
         """Validates payment method"""
+       
         
     def validate_gender():
         """Validates gender category"""
+      
         
     def validate_membership():
         """Validates memmbership category"""
+     
         
     def validate_addontotal():
         """Validates add-on_total"""
+        
     
     def validate_age():
         """Validates age values"""
+       
     
     def validate_id():
         """Validates customer id values"""
+      
     
     def validate_date():
         """Validates date values values"""
     
+    
     def validate_rating():
         """Validates rating values"""
+   
     
     def validate_totalprice():
         """Validates total price values"""
+ 
     
     def validate_unitprice():
         """Validates unit price values"""
-        
-    
-    
+   
+
