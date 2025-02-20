370ebf081f7327f61e99e28a489d566cc4a4c027
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 03:51:45 +0800

    Update orchestrator.py

--- b. etl-pipeline/orchestrator/orchestrator.py
+++ b. etl-pipeline/orchestrator/orchestrator.py
@@ -2,25 +2,24 @@ class Run(metaclass=static):
 
     @time
     @memory
-    def download():
+    def extract():
         download.authenticate_kaggle()
         download.initiate_os()
         download.download_data()
+
 
     @time
     @memory
     def transform():
         df = transform.extract_data()
+        df = transform.order_id(df)
         df = transform.delete_col(df)
         df = transform.remove_space(df)
         df = transform.lowercase_col(df)
         df = transform.strip_space(df)
         df = transform.replace_(df)
-        df = transform.save_col(df)
-
-    @time
-    @memory
-    def validate():
+        transform.save_df(df)
+        transform.sales_data(df)
         validate.validate_product()
         validate.validate_sku()
         validate.validate_order_status()
@@ -35,3 +34,13 @@ class Run(metaclass=static):
         validate.validate_rating()
         validate.validate_totalprice()
         validate.validate_unitprice()
+
+
+    @time
+    @memory 
+    def load():
+        df = convert.completed_orders()
+        df = convert.transform_price(df)
+        df = convert.transform_dates(df)
+        df = convert.create_sales(df)
+        convert.create_table(df)
