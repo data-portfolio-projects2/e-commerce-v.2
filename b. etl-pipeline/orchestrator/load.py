36197ffe391495b334c1ccd15148d23b15d470cc
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 04:06:50 +0800

    Update and rename orchestrator.py to load.py

---
+++ b. etl-pipeline/orchestrator/load.py
@@ -0,0 +1,10 @@
+
+    @time
+    @memory 
+    def load():
+        df = load.completed_orders()
+        df = load.transform_price(df)
+        df = load.transform_dates(df)
+        df = load.create_sales(df)
+        load.create_table(df)
+
