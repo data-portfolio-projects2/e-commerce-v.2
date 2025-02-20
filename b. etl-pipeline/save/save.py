839deaf09fd7fb4eceb6b6948a75e24adf07a129
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 04:17:52 +0800

    Update save.py

--- b. etl-pipeline/save/save.py
+++ b. etl-pipeline/save/save.py
@@ -1,5 +1,14 @@
+
 class DataSaver(metaclass=static):
+
+    def save_df(df):
+        """Save the dataframe as a csv file"""
+        
 
     def save_col(df):
         """Save each column as a separate csv file"""
-           
+      
+    
+    def sales_data(df):
+        """Filter sales data"""
+       
