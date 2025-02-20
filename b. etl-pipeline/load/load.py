220c103f0f05c28cec606e859d7bfac1a4274a2b
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 03:55:54 +0800

    Create load.py

---
+++ b. etl-pipeline/load/load.py
@@ -0,0 +1,33 @@
+class Converter(metaclass=static):
+
+    def completed_orders():
+        """Filter completed orders"""
+     
+
+    def transform_price(df):
+        """Transform total price values"""
+       
+    
+    def transform_dates(df):
+        """Transform dates to datetime values"""
+     
+
+    def create_sales(df):
+        """Create sales data"""
+       
+    
+    def bigquery_credentials():
+        """Authenticate BigQuery credentials"""
+     
+        
+    def create_schema():
+        """Create BigQuery schema"""
+     
+
+    def create_table(df):
+        """Uploads DataFrame to BigQuery with authentication and schema."""
+   
+
+
+
+
