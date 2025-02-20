26bd28b490b5826b1ea76df704240b616b4f40ec
data-portfolio-projects2 <my_portfolio_projects2@yahoo.com>
2025-02-21 04:18:56 +0800

    Update 1. transform.py

--- b. etl-pipeline/transform/1. transform.py
+++ b. etl-pipeline/transform/1. transform.py
@@ -1,23 +1,27 @@
-
 class DataTransformer(metaclass=static):
 
     def delete_col(df):
+     
 
     def remove_space(df):
+       
 
     def lowercase_col(df):
         """Transform columns into lowercase"""
+        
     
     def strip_space(df):
         """Remove trailing spaces in columns"""
+   
     
     def replace_(df):
         """Replacing space w/ underscore"""
+        
     
-
-
-
-    
+    def order_id(df):
+        """Adding order IDs to a Dask DataFrame"""
+        
+        
 
 
 
